b32a6d14913d2c25a853ef5dc4e185b20bb7cd66
Attica-oss <g.mounac@outlook.com>
2024-09-15 19:54:14 +0400

    changed pylint workflow to use poetry and fixed the test files

--- test/test_validation.py
+++ test/test_validation.py
@@ -9,6 +9,6 @@ def test_length_of_container_number():
     """Test if the length of the container number is 11"""
     container_number = "MSFU8626370"  # A valid 11-character container number
     is_valid = ctn.validate_container_number(container_number)
-    
+
     assert len(container_number) == 11
     assert is_valid is True
